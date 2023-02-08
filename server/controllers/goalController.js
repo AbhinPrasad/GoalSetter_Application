ded8103b1b08bc410b97f2b59700c6845dbb28b4
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-02-09 00:41:39 +0530

    added async handler

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,19 +1,21 @@
-export const getGoals = (req, res) => {
+import asyncHandler from "express-async-handler";
+
+export const getGoals = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: "get" });
-};
+});
 
-export const setGoals = (req, res) => {
-    if(!req.body.text){
-        res.status(400)
-        throw new Error('Please add a text field')
-    }
+export const setGoals = asyncHandler(async (req, res) => {
+	if (!req.body.text) {
+		res.status(400);
+		throw new Error("Please add a text field");
+	}
 	res.status(200).json({ message: "Set Goals" });
-};
+});
 
-export const updateGoals = (req, res) => {
+export const updateGoals = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: `update goal ${req.params.id}` });
-};
+});
 
-export const deleteGoals = (req, res) => {
+export const deleteGoals = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: `delete goal ${req.params.id}` });
-};
+});
