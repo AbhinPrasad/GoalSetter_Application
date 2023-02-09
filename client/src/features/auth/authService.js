2bc991ce60597f8989c5a64f5c346f8885cbc013
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-02-10 03:41:45 +0530

    extra reducers defined in auth slice

---
+++ client/src/features/auth/authService.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "/api/users/";
+
+// Register user
+const register = async (userData) => {
+	const response = await axios.post(BASE_URL, userData);
+
+	if (response.data) {
+		localStorage.setItem("user", JSON.stringify(response.data));
+	}
+
+	return response.data;
+};
+
+const authService = {
+	register
+};
+
+export default authService;
--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-expressions */
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 //Get user from localStorage
 const user = JSON.parse(localStorage.getItem("user"));
@@ -12,6 +13,24 @@ const initialState = {
 	message: ""
 };
 
+// Register user
+export const register = createAsyncThunk(
+	"auth/register",
+	async (user, thunkAPI) => {
+		try {
+			return await authService.register(user);
+		} catch (error) {
+			const message =
+				(error.response &&
+					error.response.data &&
+					error.response.data.message) ||
+				error.message ||
+				error.toString();
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
+
 export const authSlice = createSlice({
 	name: "auth",
 	initialState,
@@ -23,7 +42,23 @@ export const authSlice = createSlice({
 			state.message = "";
 		}
 	},
-	extraReducers: () => {}
+	extraReducers: (builder) => {
+		builder
+			.addCase(register.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(register.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.user = action.payload;
+			})
+			.addCase(register.rejected, (state, action) => {
+				state.isError = true;
+				state.message = action.payload;
+				state.isLoading = false;
+				state.user = null;
+			});
+	}
 });
 
 export const { reset } = authSlice.actions;
