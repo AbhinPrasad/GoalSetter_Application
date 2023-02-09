1c427fa7623883ba6e23c63b14e952b099e1de5b
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-02-10 02:13:35 +0530

    added routes in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, Login, Register } from "./pages/index";
 
 function App() {
-  return (
-    <div>
-     GoalSetter
-    </div>
-  );
+	return (
+		<>
+			<BrowserRouter>
+				<div className="container">
+					<Routes>
+						<Route path="/" element={<Dashboard />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
