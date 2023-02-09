5cf7f9fee57ebe3a60086763ccceb1e435ccd9ae
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-02-10 02:42:09 +0530

    register page finished

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
+
+const Register = () => {
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		phone: "",
+		password: "",
+		password2: ""
+	});
+
+	const { name, email, phone, password, password2 } = formData;
+
+	const onChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value
+		}));
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<>
+			<section className="heading">
+				<h1>
+					<FaUser /> Register
+				</h1>
+				<p>Please create an account</p>
+			</section>
+
+			<section className="form">
+				<form onSubmit={onSubmit}>
+					<div className="form-group">
+						<input
+							type="text"
+							className="form-control"
+							id="name"
+							name="name"
+							value={name}
+							placeholder="Full Name"
+							onChange={onChange}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="email"
+							className="form-control"
+							id="email"
+							name="email"
+							value={email}
+							placeholder="Email"
+							onChange={onChange}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="number"
+							className="form-control"
+							id="phone"
+							name="phone"
+							value={phone}
+							placeholder="Phone"
+							onChange={onChange}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							className="form-control"
+							id="password"
+							name="password"
+							value={password}
+							placeholder="Password"
+							onChange={onChange}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="text"
+							className="form-control"
+							id="password2"
+							name="password2"
+							value={password2}
+							placeholder="Confirm Password"
+							onChange={onChange}
+						/>
+					</div>
+					<div className="form-group">
+						<button type="submit" className="btn btn-block">
+							Register
+						</button>
+					</div>
+				</form>
+			</section>
+		</>
+	);
+};
+
+export default Register;
